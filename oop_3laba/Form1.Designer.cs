7f263486e6f539099f8072958a2200135826b528
Lamer Matrixxx <jssk383@gmail.com>
2021-02-25 12:37:52 +0200

    Виклав файли

--- oop_3laba/Form1.Designer.cs
+++ oop_3laba/Form1.Designer.cs
@@ -154,7 +154,7 @@ namespace oop_3laba
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(487, 223);
+            this.button1.Location = new System.Drawing.Point(461, 229);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 11;
