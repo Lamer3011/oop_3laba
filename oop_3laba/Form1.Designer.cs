3e24ca757df68de3e1f0626f4e591c7e0d96d65d
Denys <navrotskyidenys@gmail.com>
2021-03-01 15:40:13 +0200

    Виправиви помилку з подією кнопки

--- oop_3laba/Form1.Designer.cs
+++ oop_3laba/Form1.Designer.cs
@@ -160,6 +160,7 @@ namespace oop_3laba
             this.button1.TabIndex = 11;
             this.button1.Text = "Порахувати";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label4
             // 
